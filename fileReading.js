73dffaba95b527b0b305fdfc60b5c07c90261165
Pasindu Senarathne <pasindubahagya@gmail.com>
2019-03-19 22:46:54 +0530

    2nd commit

---
+++ fileReading.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const fileName = __dirname + "/test.txt";
+
+fs.readFile(fileName, (err, data) => {
+
+    if (err) {
+
+        console.log(err);
+
+    }
+
+    console.log(data.toString());
+    
+
+});
